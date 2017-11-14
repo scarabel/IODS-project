76ca87f0ecaebbbc2df5cfa3b9c12eb3648f4fd3
Francesca Scarabel <francesca.scarabel@gmail.com>
2017-11-14 23:09:32 +0200

    exercise 2
    exercise 2, tuesday

---
+++ data/create_learning2014.R
@@ -0,0 +1,50 @@
+## Francesca Scarabel, 15.11.2018
+# script create_learning2014
+#  
+# the meaning of the combined variable is available at the webpage
+# http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS2-meta.txt
+
+# load table data
+learning2014 <-  read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", header = TRUE, sep = "\t")
+
+# dimension of the table:
+dim(learning2014)
+
+# both dimension and structure:
+str(learning2014)
+
+# create new dataset
+# install.packages("dplyr")
+library(dplyr)
+
+# combine variables into deep
+deep_qu <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+deep_col <- select(learning2014, one_of(deep_qu))
+learning2014$deep <- rowMeans(deep_col)
+
+# combine variables into stra 
+stra_qu <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+stra_col <- select(learning2014,one_of(stra_qu))
+learning2014$stra <- rowMeans(stra_col)
+
+# combine variables into surf
+surf_qu <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+surf_col <- select(learning2014, one_of(surf_qu))
+learning2014$surf <- rowMeans(surf_col)
+
+# choose columns:
+columns <- c("gender","Age","Attitude","deep","stra","surf","Points")
+
+# create new dataset
+new_lrn14 <- select(learning2014,columns)
+
+# select only positive points 
+# (and check that the right number of entries is left)
+new_lrn14 <- filter(new_lrn14, Points>0)
+str(new_lrn14)
+
+# save table
+write.csv(new_lrn14, file="learning2014")
+# read table (and make sure to print all the entries)
+options(max.print=2000)
+read.csv(new_lrn14)
